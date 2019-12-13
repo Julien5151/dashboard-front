8b123f4544e74ebe921a86584fe9856a398ed650
Julien CLAUDEL <julienclaudel51@outlook.com>
2019-12-13 14:20:11 +0100

    Refactored navigation menu and styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,31 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [
+    NavigationComponent
+  ],
   imports: [
     CommonModule,
     MatMenuModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule
   ],
-  exports: [NavigationComponent]
+  exports: [
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    NavigationComponent,
+    MatToolbarModule
+  ]
 })
 export class SharedModule { }
